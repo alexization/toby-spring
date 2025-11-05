64dc4bcf4a9f29738ff8f74626e5e77f3c84bc76
hyoseok <alexization@gmail.com>
2025-11-05 18:21:11 +0900

    feat: OrderService 생성

--- src/main/java/tobyspring/hellospring/OrderClient.java
+++ src/main/java/tobyspring/hellospring/OrderClient.java
@@ -2,35 +2,17 @@ package tobyspring.hellospring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.support.TransactionCallback;
-import org.springframework.transaction.support.TransactionTemplate;
-import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
+import tobyspring.hellospring.order.OrderService;
 
 import java.math.BigDecimal;
 
-public class DataClient {
+public class OrderClient {
     public static void main(String[] args) {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
-        OrderRepository repository = beanFactory.getBean(OrderRepository.class);
-        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(OrderConfig.class);
+        OrderService service = beanFactory.getBean(OrderService.class);
 
-        try {
-            new TransactionTemplate(transactionManager).execute((TransactionCallback<Order>) status -> {
-                Order order = new Order("100", BigDecimal.TEN);
-                repository.save(order);
-
-                System.out.println(order);
-
-                Order order2 = new Order("100", BigDecimal.TEN);
-                repository.save(order2);
-
-                return null;
-            });
-        } catch (DataIntegrityViolationException e) {
-            System.out.println("DataIntegrityViolationException");
-        }
+        Order order = service.createOrder("100", BigDecimal.TEN);
+        System.out.println(order);
     }
 }
