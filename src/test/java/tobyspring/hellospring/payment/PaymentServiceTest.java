97d9e403bccf0bf33f74eb51164a121786488200
hyoseok <alexization@gmail.com>
2025-11-05 11:33:50 +0900

    feat: test code

---
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -0,0 +1,31 @@
+package tobyspring.hellospring.payment;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.TEN;
+import static java.math.BigDecimal.valueOf;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PaymentServiceTest {
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", TEN);
+
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
+    }
+
+    @Test
+    @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
+    }
+}
